39d8bab14c2e177447b3422743227bd88137d8cd
Tolevats <pstavelot@gmail.com>
2024-12-26 22:46:20 -0300

    chore: Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <strong>Tolevats</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'footer-component'
+}
+</script>
+
+<style scoped>
+.v-footer {
+    color:#fff;
+    background-color: #0b1c5b;
+}
+</style>
