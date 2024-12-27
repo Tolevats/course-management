09d2d757d1def4fed59d2823cf6a1f4b129f08ae
Tolevats <pstavelot@gmail.com>
2024-12-26 22:45:01 -0300

    chore: Add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app-bar app color="#0b1c5b" dark>
+         <v-toolbar-image>
+          <v-img
+          alt="logo"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="200"
+          src="@/assets/Logo/SVG/main-logo-white-transparent.svg"
+          width="150"
+          />
+         </v-toolbar-image>
+        <v-spacer></v-spacer>
+        <v-btn text to="/" exact>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-btn text to="/admin" exact>
+          <v-icon>mdi-wrench</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'navbar-component'
+}
+</script>
+
+<style scoped>
+</style>
