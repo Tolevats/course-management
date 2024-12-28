fff31603ff51b324d185eeccd0b15e6a80402e5f
Tolevats <pstavelot@gmail.com>
2024-12-28 01:27:00 -0300

    chore: Update router to include admin and edit course routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Admin from '../views/Admin.vue'
+import EditCourse from '@/views/EditCourse.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +10,17 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/admin',
+    name: 'admin',
+    component: Admin
+  },
+  {
+    path: '/edit/:id',
+    name: 'editCourse',
+    component: EditCourse
   }
 ]
 
