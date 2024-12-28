e36f5e06ad81336df7972dc6ba1035fc2db5bec9
Tolevats <pstavelot@gmail.com>
2024-12-28 01:20:48 -0300

    chore: Add edit course view

---
+++ src/views/EditCourse.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Edit Course</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'editCourse-view'
+  // props: {},
+/*     data: function(){
+        return {}
+    }, */
+  // computed: {},
+  // methods: {}
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+</style>
