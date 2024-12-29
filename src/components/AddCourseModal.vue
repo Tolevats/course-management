2729002534b785809c37958a926fb5b9e03da748
Tolevats <pstavelot@gmail.com>
2024-12-29 18:52:53 -0300

    update: Refactor AddCourseModal to use 'value' prop

--- src/components/AddCourseModal.vue
+++ src/components/AddCourseModal.vue
@@ -1,88 +1,88 @@
 <template>
-  <v-container>
-    <!-- isDialogOpen mirrors the isOpen prop to manage the dialog's state internally -->
-    <v-dialog v-model="isDialogOpen" max-width="600px">
-      <v-card>
-        <v-card-title>
-          <span class="headline">Add New Course</span>
-        </v-card-title>
-        <!-- form -->
-        <v-card-text>
-          <v-form ref="addCourseForm" v-model="isValid">
-            <v-text-field
-              label="Course Name"
-              v-model="course.name"
-              :rules="[v => !!v || 'Name is required']"
-            />
-            <v-text-field
-              label="Image URL"
-              v-model="course.imageUrl"
-              :rules="[v => !!v || 'Image URL is required']"
-            />
-            <v-text-field
-              label="Places"
-              v-model.number="course.places"
-              type="number"
-              :rules="[v => v > 0 || 'Must be greater than 0']"
-            />
-            <v-text-field
-              label="Enrolled"
-              v-model.number="course.enrolled"
-              type="number"
-              :rules="[v => v <= course.places || 'Enrolled must not exceed places']"
-            />
-            <v-text-field
-              label="Duration"
-              v-model="course.duration"
-            />
-            <v-text-field
-              label="Cost"
-              v-model.number="course.cost"
-              type="number"
-            />
-            <v-textarea
-              label="Description"
-              v-model="course.description"
-              rows="3"
-            />
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="grey" text @click="closeDialog">Cancel</v-btn>
-          <v-btn color="primary" :disabled="!isValid" @click="addCourse">Add</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-container>
+  <!-- isDialogOpen mirrors the isOpen prop to manage the dialog's state internally -->
+  <v-dialog v-model="isDialogOpen" max-width="600px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Add New Course</span>
+      </v-card-title>
+      <!-- form -->
+      <v-card-text>
+        <v-form ref="addCourseForm" v-model="isValid">
+          <v-text-field
+            label="Course Name"
+            v-model="course.name"
+            :rules="[v => !!v || 'Name is required']"
+          />
+          <v-text-field
+            label="Image URL"
+            v-model="course.imageUrl"
+            :rules="[v => !!v || 'Image URL is required']"
+          />
+          <v-text-field
+            label="Places"
+            v-model.number="course.places"
+            type="number"
+            :rules="[v => v > 0 || 'Must be greater than 0']"
+          />
+          <v-text-field
+            label="Enrolled"
+            v-model.number="course.enrolled"
+            type="number"
+            :rules="[v => v <= course.places || 'Enrolled must not exceed places']"
+          />
+          <v-text-field
+            label="Duration"
+            v-model="course.duration"
+          />
+          <v-text-field
+            label="Cost"
+            v-model.number="course.cost"
+            type="number"
+          />
+          <v-textarea
+            label="Description"
+            v-model="course.description"
+            rows="3"
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="grey" text @click="closeDialog">Cancel</v-btn>
+        <v-btn color="success" :disabled="!isValid" @click="addCourse">Add</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
   props: {
-    isOpen: {
+    value: {
       type: Boolean,
       required: true
     }
   },
-  data: () => ({
-    isValid: false,
-    isDialogOpen: false, // Local property to mirror the isOpen prop
-    course: {
-      name: '',
-      imageUrl: '',
-      places: 0,
-      enrolled: 0,
-      duration: '',
-      cost: 0,
-      description: ''
+  data () {
+    return {
+      isValid: false,
+      isDialogOpen: this.value, // initialize from prop
+      course: {
+        name: '',
+        imageUrl: '',
+        places: 0,
+        enrolled: 0,
+        duration: '',
+        cost: 0,
+        description: ''
+      }
     }
-  }),
+  },
   watch: {
-    isOpen (newVal) { // ensures the local property is updated when the parent changes the isOpen prop
-      this.isDialogOpen = newVal // Keep in sync when the prop changes
+    value (newVal) { // ensures the local property is updated when the parent changes the isOpen prop
+      this.isDialogOpen = newVal // keep in sync when the parent prop changes
     },
     isDialogOpen (newVal) { // emits updates to the parent when the local property is changed
-      this.$emit('update:isOpen', newVal) // Notify parent of changes
+      this.$emit('input', newVal) // emit update to parent
     }
   },
   methods: {
